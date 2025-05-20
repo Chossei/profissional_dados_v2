cee0702647f1a756590fd439997b36ebada65246
Átila Prudente <150717036+Chossei@users.noreply.github.com>
2025-05-20 19:08:39 +0000

    asasas

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ st.set_page_config(page_title='Atividade 1 - Análise de Regressão', layout='ce
 
 paginas = {
     'Análise de dados': [st.Page('paginas/app2.py', title='Dashboard Interativo da Faixa Salarial', default=True),
-                        st.Page('paginas/cientista.py', title = 'Sobre o Cientista de Dados', default=False)]
+                        st.Page('paginas/cientista.py', title = 'Sobre o Cientista de Dados', default=False),
+                        st.Page('paginas/teste.py', title = 'Programação em quatro mãos, 20 dedos', default=False)]
 }
 
 pag = st.navigation(paginas)
